bbd1a1c4fde39c3200d6081d817ae3f1d348af2d
Ravi Ahirwar <ra6614967@gmail.com>
2026-06-07 18:26:55 +0530

    Fifth commit

--- index.js
+++ index.js
@@ -12,14 +12,17 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
     {
+        id:"1a",
         username: "Ravi Ahirwar",
         content: "I am working on software work"
     },
     {
-        username: "Neshu Kumari",
+        id:"2b",
+        username: "Nishu Kumari",
         content: "I am working on applicatin on work"
     },
     {
+        id:"3c",
         username: "Satyam Verma",
         content: "I am working on website work"
     }
@@ -28,7 +31,6 @@ let posts = [
 app.get("/posts", (req, res) => {
     res.render("index.ejs", { posts });
 });
-
 
 app.get("/posts/new", (req,res) => {
     res.render("new.ejs")
@@ -39,6 +41,15 @@ app.post("/posts", (req,res) =>{
     posts.push({username,content});
     res.redirect("/posts");
 });
+
+app.get("/posts/:id", (req,res) =>{
+    let {id}= req.params;
+    console.log(id);
+    let post=posts.find((p)=> id ===p.id);
+    res.render("show.ejs", { post });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
