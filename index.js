d999df8554461d36a17c63482cd41aa8aee44305
Ravi Ahirwar <ra6614967@gmail.com>
2026-06-07 14:34:39 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let posts = [
+    {
+        username: "Ravi Ahirwar",
+        content: "I am working on software work"
+    },
+    {
+        username: "Neshu Kumari",
+        content: "I am working on applicatin on work"
+    },
+    {
+        username: "Satyam Verma",
+        content: "I am working on website work"
+    }
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
+});
+
+
+app.get("/posts/new", (req,res) => {
+    res.render("new.ejs")
+})
+
+app.post("/posts", (req,res) =>{
+    let{username,content}=req.body;
+    posts.push({username,content});
+    res.send("post request working");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+});
